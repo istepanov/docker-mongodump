ed9389610db6cb5e4f838ea22c5d0e314b3dc6ec
Ilya Stepanov <dev@ilyastepanov.com>
2014-12-20 00:16:22 -0800

    Backup filename fix

--- backup.sh
+++ backup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DATE=$(date +%Y%m%d_%H%M%S)
-FILE="/backup/backup-$date.tar.gz"
+FILE="/backup/backup-$DATE.tar.gz"
 
 mongodump --quiet -h $MONGO_PORT_27017_TCP_ADDR -p $MONGO_PORT_27017_TCP_PORT
 tar -zcvf $FILE dump/
