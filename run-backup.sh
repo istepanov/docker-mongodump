41d40a778c5ba0f55c1113963681deb9d31171fa
Ivan Ermilov <earthquakesan@gmail.com>
2015-08-11 12:59:31 +0200

    changed the name of the docker image to the original one

--- run-backup.sh
+++ run-backup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 CONTAINER_NAME=$1
+DOCKER_NAME="istepanov/mongodump"
 
-docker run --rm -v $DIR:/backup --link $CONTAINER_NAME:mongo mongobackup no-cron
+docker run --rm -v $DIR:/backup --link $CONTAINER_NAME:mongo $DOCKER_NAME no-cron
