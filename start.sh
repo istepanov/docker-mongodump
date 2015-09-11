b119070e486b784b3da19760d624859fa837a39e
Ilya Stepanov <dev@ilyastepanov.com>
2015-09-10 20:18:45 -0700

    Make sure cron.log exists

--- start.sh
+++ start.sh
@@ -9,6 +9,7 @@ CRON_SCHEDULE=${CRON_SCHEDULE:-0 1 * * *}
 if [[ "$1" == 'no-cron' ]]; then
     exec /backup.sh
 else
+    touch /var/log/cron.log
     echo "$CRON_SCHEDULE /backup.sh >> /var/log/cron.log 2>&1" | crontab -
     exec cron -f
 fi
