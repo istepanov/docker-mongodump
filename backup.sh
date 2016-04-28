c4e1032a73663ada87bd7f9b830cd5c5728129b5
Dmitriy Voeykov <dmitriyv@pushok.com>
2016-04-28 18:42:34 +0300

    hotfix for missing spaces in bash condition

--- backup.sh
+++ backup.sh
@@ -9,8 +9,8 @@ FILE="/backup/backup-$DATE.tar.gz"
 
 arr=("--quiet" "-h" "$MONGO_PORT_27017_TCP_ADDR" "-p" "$MONGO_PORT_27017_TCP_PORT")
 if [ ! -z $MONGO_DB_NAME ]; then
-  arr+=("-d")
-  arr+=("$MONGO_DB_NAME")
+	arr+=("-d")
+	arr+=("$MONGO_DB_NAME")
 fi
 mongodump "${arr[@]}"
 tar -zcvf $FILE dump/
@@ -21,13 +21,15 @@ FILES=/backup/*.tar.gz
 currtime=`date +%s`
 for f in $FILES
 do
-	if [-f $f];
+	if [ -f $f ];
 	then
 		filemtime=`stat -c %Y $f`
 		diff=$(( (currtime - filemtime)/86400))
 		if (($diff > $MONGO_BACKUP_EXPIRE_DAYS));
 		then
+			echo "deleting '$f' ..."
 			rm $f
+			echo "deleted."
 		fi
 	fi
 done
