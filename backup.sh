bf67ac37b3bf0c7fd729d74c48f07de93e292046
tenorok <mail@tenorok.ru>
2017-02-17 21:26:44 +0300

    Explicitly set output file for dump.

--- backup.sh
+++ backup.sh
@@ -6,9 +6,10 @@ echo "Job started: $(date)"
 
 DATE=$(date +%Y%m%d_%H%M%S)
 FILE="/backup/backup-$DATE.tar.gz"
+OUTPUT="dump/"
 
-mongodump --quiet -h $MONGO_PORT_27017_TCP_ADDR -p $MONGO_PORT_27017_TCP_PORT
-tar -zcvf $FILE dump/
-rm -rf dump/
+mongodump --quiet -h $MONGO_PORT_27017_TCP_ADDR -p $MONGO_PORT_27017_TCP_PORT -o $OUTPUT
+tar -zcvf $FILE $OUTPUT
+rm -rf $OUTPUT
 
 echo "Job finished: $(date)"
