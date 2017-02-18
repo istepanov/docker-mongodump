29c4922fba09e4d10dbd6553c7abef3b94333eb5
tenorok <mail@tenorok.ru>
2017-02-18 18:50:56 +0300

    Create variable MONGO_DB_NAMES.

--- backup.sh
+++ backup.sh
@@ -8,12 +8,26 @@ DATE=$(date +%Y%m%d_%H%M%S)
 DIR="/backup"
 
 if [[ $BACKUP_FILE_NAME ]]; then
-    FILE="$DIR/$BACKUP_FILE_NAME.tar.gz"
+    FILE="$DIR/$BACKUP_FILE_NAME"
 else
-    FILE="$DIR/$DATE.tar.gz"
+    FILE="$DIR/$DATE"
 fi
 
-mongodump --quiet -h $MONGO_PORT_27017_TCP_ADDR -p $MONGO_PORT_27017_TCP_PORT --archive=$FILE --gzip
+command="mongodump --quiet -h $MONGO_PORT_27017_TCP_ADDR -p $MONGO_PORT_27017_TCP_PORT --gzip"
+
+if [[ $MONGO_DB_NAMES ]]; then
+    dbs=( $MONGO_DB_NAMES )
+
+    for d in ${dbs[@]}
+    do
+        if [[ ! $BACKUP_FILE_NAME ]]; then
+            filename="$FILE-$d"
+        fi
+        eval $command " --archive=$filename -d $d"
+    done
+else
+    eval $command " --archive=$FILE"
+fi
 
 if [[ $BACKUP_EXPIRE_DAYS ]]; then
     echo "Removing backups older than $BACKUP_EXPIRE_DAYS days"
