3edc4b707adf6a0b9da9666c845a46d71ee5d72f
tenorok <mail@tenorok.ru>
2017-02-18 15:16:59 +0300

    Create variable BACKUP_FILE_NAME.

--- backup.sh
+++ backup.sh
@@ -6,7 +6,12 @@ echo "Job started: $(date)"
 
 DATE=$(date +%Y%m%d_%H%M%S)
 DIR="/backup"
-FILE="$DIR/$DATE.tar.gz"
+
+if [[ $BACKUP_FILE_NAME ]]; then
+    FILE="$DIR/$BACKUP_FILE_NAME.tar.gz"
+else
+    FILE="$DIR/$DATE.tar.gz"
+fi
 
 mongodump --quiet -h $MONGO_PORT_27017_TCP_ADDR -p $MONGO_PORT_27017_TCP_PORT --archive=$FILE --gzip
 
