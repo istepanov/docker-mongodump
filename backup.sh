725f82492b52a45608eed4b17e8a2297f5899531
ScyDev <lukas.saegesser@gmail.com>
2017-03-13 13:17:51 +0100

    - make it possible to configure db to dump

--- backup.sh
+++ backup.sh
@@ -8,9 +8,9 @@ echo "deleting files older than 30 days"
 find /backup -mtime +30 -type f -delete
 
 DATE=$(date +%Y%m%d_%H%M%S)
-FILE="/backup/backup-$DATE.tar.gz"
+FILE="/backup/$MONGO_DB_NAME-backup-$DATE.tar.gz"
 
-mongodump --quiet -h $MONGO_PORT_27017_TCP_ADDR -p $MONGO_PORT_27017_TCP_PORT
+mongodump --quiet -h $MONGO_PORT_27017_TCP_ADDR -p $MONGO_PORT_27017_TCP_PORT --db=$MONGO_DB_NAME
 tar -zcvf $FILE dump/
 rm -rf dump/
 
