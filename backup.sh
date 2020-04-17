ea3d6e619904d2847e311248407e3cac35bcbd25
Ilya Stepanov <dev@ilyastepanov.com>
2020-04-16 21:00:00 -0700

    Add ability to upload backups to S3.

--- backup.sh
+++ backup.sh
@@ -8,8 +8,14 @@ DATE=$(date +%Y%m%d_%H%M%S)
 FILE="/backup/backup-$DATE.tar.gz"
 
 mkdir -p dump
-mongodump -h $MONGO_HOST -p $MONGO_PORT
-tar -zcvf $FILE dump/
+mongodump --uri "MONGO_URI"
+tar -zcvf "$FILE" dump/
 rm -rf dump/
+
+if [[ "$TARGET_S3_FOLDER" ]]; then
+    aws s3 cp "$FILE" "$TARGET_S3_FOLDER"
+    echo "$FILE uploaded to $TARGET_S3_FOLDER"
+    rm -rf "$FILE"
+fi
 
 echo "Job finished: $(date)"
---
+++ entrypoint.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -e
+
+export MONGO_URI=${MONGO_URI:-mongodb://mongo:27017}
+
+# Optional env vars:
+# - CRON_SCHEDULE
+# - TARGET_S3_FOLDER
+# - AWS_ACCESS_KEY_ID
+# - AWS_SECRET_ACCESS_KEY
+
+if [[ "$CRON_SCHEDULE" ]]; then
+    LOGFIFO='/var/log/cron.fifo'
+    if [[ ! -e "$LOGFIFO" ]]; then
+        mkfifo "$LOGFIFO"
+    fi
+    CRON_ENV="MONGO_URI='$MONGO_URI'"
+    if [[ "$TARGET_S3_FOLDER" ]]; then
+        CRON_ENV="$CRON_ENV\nTARGET_S3_FOLDER='$TARGET_S3_FOLDER'"
+    fi
+    if [[ "$AWS_ACCESS_KEY_ID" ]]; then
+        CRON_ENV="$CRON_ENV\nAWS_ACCESS_KEY_ID='$AWS_ACCESS_KEY_ID'"
+    fi
+    if [[ "$AWS_SECRET_ACCESS_KEY" ]]; then
+        CRON_ENV="$CRON_ENV\nAWS_SECRET_ACCESS_KEY='$AWS_SECRET_ACCESS_KEY'"
+    fi
+    echo -e "$CRON_ENV\n$CRON_SCHEDULE /backup.sh > $LOGFIFO 2>&1" | crontab -
+    crontab -l
+    cron
+    tail -f "$LOGFIFO"
+else
+    exec /backup.sh
+fi
